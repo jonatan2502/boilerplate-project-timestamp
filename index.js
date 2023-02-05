9e3d94a6d293b0b45d58d03fddff3c668c785a35
Jonatan P <jonatan2502@gmail.com>
2023-02-05 12:22:47 -0400

    GET /:date? route working

--- index.js
+++ index.js
@@ -6,27 +6,47 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/:date?', function (req, res) {
+	let { date } = req.params;
+	if (!date) {
+		let date = new Date();
+		let unix = date.getTime() / 1000;
+		return res.json({
+			unix,
+			utc: date,
+		});
+	} else if (!date.includes('-')) {
+		let unix = parseInt(req.params.date) * 1000;
+		let date = new Date(unix);
+		return res.json({
+			unix,
+			utc: date,
+		});
+	} else {
+		let date = new Date(req.params.date);
+		let unix = date.getTime() / 1000;
+		return res.json({
+			unix,
+			utc: date,
+		});
+	}
 });
-
-
 
 // listen for requests :)
+// var listener = app.listen(3001, function () {
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+	console.log('Your app is listening on port ' + listener.address().port);
 });
